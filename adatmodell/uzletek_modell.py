3760fbe4721eae671b1a359d92ead8eb38b22096
szelesg76 <szelesg76@gmail.com>
2024-05-31 20:41:27 +0200

    adatbázis változások

--- adatmodell/uzletek_modell.py
+++ adatmodell/uzletek_modell.py
@@ -1,8 +1,16 @@
 from adatbazis.adatbazis import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, BIGINT, VARCHAR, INT
+from sqlalchemy.orm import Mapped, mapped_column
 
+# class Uzletek(Base):
+#     __tablename__ = 'uzletek'
+    
+#     uzlet_id  = Column(Integer, primary_key=True, index=True)
+#     uzlet_nev = Column(String)
+    
+    
 class Uzletek(Base):
     __tablename__ = 'uzletek'
     
-    uzlet_id  = Column(Integer, primary_key=True, index=True)
-    uzlet_nev = Column(String)
+    uzlet_id: Mapped[int]  = mapped_column(INT, primary_key=True, index=True, autoincrement=True)
+    uzlet_nev: Mapped[str] = mapped_column(VARCHAR(255), nullable=False)
